86f29af4e3429facf2cac23bf039da2fbdafec60
Alan Ronaldo <al222310501@gmail.com>
2026-09-18 09:53:15 -0600

    feat: configurar URL de API para producción

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
-// src/services/api.js
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080/api",
 });
 
 export default api;
